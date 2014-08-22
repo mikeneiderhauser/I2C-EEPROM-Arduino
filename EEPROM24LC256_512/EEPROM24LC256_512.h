b7387a30315be5150f3f840b22624b450d809810
Riley Wood <rjw245@gmail.com>
2014-08-22 19:54:40 -0400

    Accidentally replaced the .h with the .cpp in last commit

--- EEPROM24LC256_512/EEPROM24LC256_512.h
+++ EEPROM24LC256_512/EEPROM24LC256_512.h
@@ -12,178 +12,57 @@
   This library is to be used "as-is" without warranty. Please report
   bugs to the GitHub Project.
   
-  Written by Michael Neiderhauser. October 10, 2012
+  Written by Michael Neiderhauser.  October 10, 2012
   LGPL license, all text above must be included in any redistribution
  ****************************************************/
 
-#ifndef EEPROM24LC256_512_C
-#define EEPROM24LC256_512_C
+#ifndef EEPROM24LC256_512_H
+#define EEPROM24LC256_512_H
 
-#include "Arduino.h"
-#include <Wire.h>
-#include "EEPROM24LC256_512.h"
+#define PAGE256 64
+#define PAGE512 128
+#define MINIBUFFER 16
+#define MODE_256 0
+#define MODE_512 1
+#define I2CBASEADDR 0x50
 
-EEPROM256_512::EEPROM256_512()
+class EEPROM256_512
 {
-	uint8_t i=0;
-	for(i=0;i<MINIBUFFER;i++){minibuffer[i]=0b00000000;}
-	page_size = 256;
-	fulladdr=I2CBASEADDR;
-}
-
-EEPROM256_512::~EEPROM256_512(){/*Nothing to deconstruct*/}
-
-void EEPROM256_512::begin(uint8_t addr, uint8_t mode)
-{
-	if(addr > 7) addr=7;
-    fulladdr = I2CBASEADDR|addr;
-	if(mode==MODE_512)
-	{
-		page_size=PAGE512;
-	}
-	else //mode==MODE_256
-	{
-		//assume default as 256
-		page_size=PAGE256;
-	}
-}//end begin
-
-int EEPROM256_512::getPageSize()
-{
-	return page_size;
-}//end get page size
-
-uint8_t EEPROM256_512::readByte(unsigned int addr)
-{
-	uint8_t data = 0xFF;//set return byte to 0xFF or (255 DEC) or (11111111 BIN)
-    Wire.beginTransmission(fulladdr);//begin transmission on the wire
-    //32768 bytes... addressed by 2 bytes (16bits)
-    Wire.write((int)(addr >> 8));   // MSB write upper 8 bits to wire (by bit shifting to right)
-    Wire.write((int)(addr & 0xFF)); // LSB write lower 8 bits to wire (by bit & by 0xFF)
-    Wire.endTransmission();//end the transmission on the wire
-    Wire.requestFrom((uint8_t)fulladdr,(uint8_t)1);//request 1 byte from EEPROM (memory location sent above)
-    if (Wire.available()) data = Wire.read();
-    return data;
-}//end readByte
-
-void EEPROM256_512::writeByte(unsigned int addr, uint8_t data)
-{
-	Wire.beginTransmission(fulladdr);//begin transmission on wire
-    //32768 bytes... addressed by 2 bytes (16bits)
-    Wire.write((int)(addr >> 8));   // MSB write upper 8 bits to wire (by bit shifting to right)
-    Wire.write((int)(addr & 0xFF)); // LSB write lower 8 bits to wire (by bit & by 0xFF)
-    Wire.write(data);//write data byte
-    Wire.endTransmission();//end transmission on wire
-    delay(5);//wait max write time of chip
-}//end writeByte
-
-void EEPROM256_512::readPage(unsigned int page_number, uint8_t* data)
-{
-	//assuming that the char* data is the size of a page
-    uint8_t index = 0;
-    unsigned int lower_addr=0;
-    unsigned int addr_base = 0;
-    uint8_t itr=0;
-    uint8_t offset = 0;
-    
-    if(page_number < 0){page_number=0;}   
-    else if(page_number > 511)
-    {page_number = 511;}
+public:
+	EEPROM256_512();//constructor
+	~EEPROM256_512();//deconstructor
+	void begin(uint8_t addr, uint8_t mode);
+	uint8_t readByte(unsigned int addr);
+	void writeByte(unsigned int addr, uint8_t data);
+	void readPage(unsigned int page_number, uint8_t* data);
+	void writePage(unsigned int page_number, uint8_t* data);
+	int getPageSize();
+	uint8_t fulladdr;
 	
-    addr_base = page_number * page_size;
-    for(itr=0;itr<((page_size)/(MINIBUFFER)); itr++)
-    {
-		offset = (MINIBUFFER*itr);
-		lower_addr = addr_base + offset;
-		read16Byte(lower_addr);
-		for(index=offset;index<((MINIBUFFER)+offset);index++)
-		{
-			data[index] = minibuffer[(index-offset)];
-		}//end copy loop
-    }//end itr loop
-}//end readPage
+private:
+	void read16Byte(unsigned int addr);
+	void write16Byte(unsigned int addr);
+	uint8_t minibuffer[MINIBUFFER];
+	int page_size;	
+};//end class
 
-void EEPROM256_512::writePage(unsigned int page_number, uint8_t* data)
+
+class EEPROM256 : public EEPROM256_512
 {
-	//assuming the data* is size of a page
-    uint8_t index = 0;
-    unsigned int lower_addr=0;
-    unsigned int addr_base=0;
-    uint8_t itr = 0;
-    uint8_t offset = 0;
+public:
+	EEPROM256(); //constructor
+	~EEPROM256(); //deconstructor
+	void begin(uint8_t addr);
+};//end class
 
-    //check page out of bounds of chip memory
-    if(page_number < 0)
-    {page_number=0;}   
-    else if(page_number > 511)
-    {page_number = 511;}
-    
-    addr_base = page_number * page_size;
-    
-    for(itr=0;itr<((page_size)/(MINIBUFFER)); itr++)
-    {
-		offset = (MINIBUFFER*itr);
-		lower_addr = addr_base + offset;
-		for(index=offset;index<((MINIBUFFER)+offset);index++)
-		{
-			minibuffer[(index-offset)] = data[index];
-		}//end copy loop 
-		write16Byte(lower_addr);
-	}//end itr loop
-}//end writePage
 
-void EEPROM256_512::read16Byte(unsigned int addr)
+class EEPROM512 : public EEPROM256_512
 {
-    uint8_t ct = 0;
-    //assuming char* data is 16 byte buffer
-    Wire.beginTransmission(fulladdr);//begin transmission on the wire
-    //32768 bytes... addressed by 2 bytes (16bits)
-    Wire.write((int)(addr >> 8));   // MSB write upper 8 bits to wire (by bit shifting to right)
-    Wire.write((int)(addr & 0xFF)); // LSB write lower 8 bits to wire (by bit & by 0xFF)
-    Wire.endTransmission();//end the transmission on the wire
-    	
-    Wire.requestFrom((uint8_t)fulladdr,(uint8_t)16);//request 1 byte from EEPROM (memory location sent above)
-    while (Wire.available())
-    {
-        minibuffer[ct] = Wire.read();
-        ct++;
-    }
-}//end read16Byte
-
-void EEPROM256_512::write16Byte(unsigned int addr)
-{
-	uint8_t ct=0;
-    Wire.beginTransmission(fulladdr);//begin transmission on wire
-    //32768 bytes... addressed by 2 bytes (16bits)
-    Wire.write((int)(addr >> 8));   // MSB write upper 8 bits to wire (by bit shifting to right)
-    Wire.write((int)(addr & 0xFF)); // LSB write lower 8 bits to wire (by bit & by 0xFF)
-    while(ct<16)
-    {
-		Wire.write((uint8_t)minibuffer[ct]);
-		ct++;
-    }  
-    Wire.endTransmission();
-	delay(5);
-}//end write16Byte
-
-//512 class
-EEPROM512::EEPROM512(): EEPROM256_512(){/*Use base class constructor*/}
-
-EEPROM512::~EEPROM512() {/*Nothing to deconstruct*/}
-
-void EEPROM512::begin(uint8_t addr){
-	EEPROM256_512::begin(addr,MODE_512);
-}
-
-//256 class
-EEPROM256::EEPROM256(): EEPROM256_512(){/*Use base class constructor*/}
-
-EEPROM256::~EEPROM256() {/*Nothing to deconstruct*/}
-
-void EEPROM256::begin(uint8_t addr){
-	EEPROM256_512::begin(addr,MODE_256);
-}
-
+public:
+	EEPROM512(); //constructor
+	~EEPROM512(); //deconstructor
+	void begin(uint8_t addr);
+};//end class
 
 
 #endif
